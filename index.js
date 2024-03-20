c8f920d1714858772d63455caf4e7f41b1d1bbad
elizabeth lands <elizabeth_lands@hotmail.com>
2024-03-20 12:10:33 +0000

    feat: fetch countries

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
-const countries = [
-  {
-    name: "England",
-    capital: "London",
-    population: 69000,
-  },
-
-  {
-    name: "Latvia",
-    capital: "Riga",
-    population: 15000,
-  },
-
-  {
-    name: "Finland",
-    capital: "Helsinki",
-    population: 10000,
-  },
-];
-
 const container = document.getElementById("container");
+const url = "https://restcountries.com/v3.1/subregion/Northern Europe";
 
-//get one country name
-container.innerHTML = `<h1>${countries[1].name}</h1>`;
-
-//set up country cards
-const countryCards = (item) => {
-  return `<div class="country-card">
-    <h2>${item.name}</h2>
-    <p>${item.capital}</p>
-    <p>${item.population}</p>
-  </div>`;
+const createCountryCard = (item) => {
+  return `
+<div class="country-card">
+  <h3>${item.name.common}</h3>
+  <p>Capital: ${item.capital}</p>
+  <p>Population: ${item.population}</p>
+</div>`;
 };
 
-container.innerHTML = countries.map(countryCards).join("");
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    container.innerHTML = data.map(createCountryCard).join("");
+  })
+  .catch((error) => console.log("error", error));
